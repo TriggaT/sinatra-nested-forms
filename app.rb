6bb50b959f38db2d64a16fca6d6fd99664e88636
Trayshawn <traytwebb14@yahoo.com>
2020-12-23 02:06:12 -0600

    commit tree 28dc6962b91174e65a321aae312cf9c5e0aa90a4

--- app.rb
+++ app.rb
@@ -5,5 +5,36 @@ module FormsLab
 
     # code other routes/actions here
 
-  end
+    get '/' do 
+      erb :root
+    end 
+
+    get "/new" do 
+      erb :'pirates/new'
+
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate][:name],params[:pirate][:weight], params[:pirate][:height])
+
+      
+      params[:pirate][:ships].map do |details|
+        
+        Ship.new(details[:name], details[:type], details[:booty])
+    
+      end
+
+     
+
+      
+      
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
+
+
+
+  end 
 end
